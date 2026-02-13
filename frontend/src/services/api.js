1ad9c5f1ac7fa3abe5701985d216b8f328a0db97
esirisr <esirsir123@gmail.com>
2026-02-14 00:43:12 +0300

    Change API base URL to mybe.up.railway.app

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: ''https://himilos.up.railway.app'' });
+const api = axios.create({ baseURL: ''https://mybe.up.railway.app/'' });
 
 export default api
