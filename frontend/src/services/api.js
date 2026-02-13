c87b83fc7b1c7f837d632f3dbe9949755f6e37f1
esirisr <esirsir123@gmail.com>
2026-02-14 02:00:54 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ 
-  // ADD /api to the end of your URL
-  baseURL: "https://mybe.up.railway.app/api", 
-  withCredentials: true // This must be true to match your backend CORS 'credentials: true'
-});
+const api = axios.create({ baseURL: 'https://mybe.up.railway.app/api/' });
 
 export default api;
