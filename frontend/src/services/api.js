1c4babf8a4c326984c5406b34cb292ff150121fe
esirisr <esirsir123@gmail.com>
2026-02-14 00:21:18 +0300

    Update API base URL to production environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,4 @@ import axios from 'axios';
 #const api = axios.create({ baseURL: 'http://localhost:5000/api' });
 const api = axios.create({ baseURL: ''https://mybe-production-6cc4.up.railway.app/api'' });
 
-
-https://mybe-production-6cc4.up.railway.app
-
 export default api
