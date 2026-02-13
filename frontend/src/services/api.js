160dd08379004bdd89fe85982c3595c5a11e9db6
esirisr <esirsir123@gmail.com>
2026-02-14 00:21:02 +0300

    Update API base URL for production environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 #const api = axios.create({ baseURL: 'http://localhost:5000/api' });
-const api = axios.create({ baseURL: 'https://himilos.up.railway.app/api/students' });
+const api = axios.create({ baseURL: ''https://mybe-production-6cc4.up.railway.app/api'' });
+
 
 https://mybe-production-6cc4.up.railway.app
 
