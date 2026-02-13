58f82362a66fcee143bc91d719521baaa87ea284
esirisr <esirsir123@gmail.com>
2026-02-14 00:15:57 +0300

    Update API base URL for student service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 #const api = axios.create({ baseURL: 'http://localhost:5000/api' });
-const api = axios.create({ baseURL: 'https://mybe-production-6cc4.up.railway.appi' });
+const api = axios.create({ baseURL: 'https://himilos.up.railway.app/api/students' });
 
 https://mybe-production-6cc4.up.railway.app
 
